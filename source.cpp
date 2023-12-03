e883c530f7c5486d0d0d10eca67c8f1b7c1b89c2
Jacob Domagala <domagala.jacob@gmail.com>
2023-12-03 15:01:25 +0100

    Update source

--- source.cpp
+++ source.cpp
@@ -1,3 +1,42 @@
-int main(int /*argc*/, char** /*argv*/){
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Example {
+    int* ptr;
+
+public:
+    Example() : ptr(new int(42)) {}  // Memory leak potential
+
+    void setPtr(int* p) {
+        delete ptr;  // Potential double delete
+        ptr = p;
+    }
+
+    ~Example() {
+        delete ptr;  // Potential double delete
+    }
+};
+
+int divide(int a, int b) {
+    if (b == 0) {
+        std::cout << "Division by zero!" << std::endl;  // Exception not thrown
+        return -1;
+    }
+    return a / b;
+}
+
+int main() {
+    Example ex;
+    int x = 10;
+    int y = 0;
+    std::cout << divide(x, y) << std::endl;  // Divide by zero at runtime
+
+    int* ptr = nullptr;
+    std::cout << *ptr << std::endl;  // Dereferencing null pointer
+
+    std::vector<int> vec;
+    std::cout << vec[10] << std::endl;  // Out of bounds vector access
+
     return 0;
 }
